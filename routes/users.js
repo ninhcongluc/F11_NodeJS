0f3638d8fa3ec3b1f5cd291d4143301a943b312a
ninhcongluc <ninhcongluc>
2021-09-09 17:44:04 +0700

    Design RestApi work with Mock database

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { getUsers, createUser, getUser, deleteUser, updateUser } from '../controllers/users.js'
+
+const router = express.Router() 
+
+
+// all routes in here starting with slash /users
+router.get('/', getUsers)
+
+router.post('/', createUser)
+
+router.get('/:id', getUser)
+
+router.delete('/:id' , deleteUser)
+
+router.patch('/:id' , updateUser)
+
+
+export default router
